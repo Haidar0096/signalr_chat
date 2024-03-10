feadd638581b8f03115d3580f16b0cc9c0128aeb
Haidar <haidarrmehsen@gmail.com>
2024-03-10 18:53:47 +0200

    commit

--- obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
+++ obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b97a4107dd842c0f0c8bf20e55a075b610bf3d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
